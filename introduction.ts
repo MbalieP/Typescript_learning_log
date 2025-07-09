3a0ef3aa1c3c507351821b4da2f67a92a7e3ede0
mbali phulwane <mbphulwjhb024@student.wethinkcode.co.za>
2025-07-09 06:11:13 +0200

    introduction

--- introduction.ts
+++ introduction.ts
@@ -1,1 +1,27 @@
-intro
+const menu = [
+    {name: "morazarella", price: 8 },
+    {name: "pepperoni", price: 10},
+    {name: "veggie", price: 12},
+    {name: "hawaiian", price: 14}
+]
+
+const cashInRegister = 100
+const orderQueue = []
+
+
+function addNewPizza(pizzaObj){
+    menu.push(pizzaObj)
+
+}
+function placeOrder(pizzaName){
+    const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName);
+
+    if(!selectedPizza){return null};
+
+    cashInRegister += selectedPizza.price;
+
+    const NewOrder = {pizza: selectedPizza, status : "ordered"};
+    orderQueue.push(newOrder);
+    return newOrder;  
+}
+function completeOrder()
