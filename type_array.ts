1c3802fac832019e9235d8d37f5c59eeed07af99
mbali phulwane <mbphulwjhb024@student.wethinkcode.co.za>
2025-07-11 07:18:53 +0200

    pizza

--- type_array.ts
+++ type_array.ts
@@ -1,7 +1,7 @@
 let age : number = 100
 
 let ages:number[] = [20,100]
-let names: string[] = ["mbali","petty","thoko"]
+let names: string[] = ["mbali","petty","thoko","Lebo"]
 
 //let people:Person =[person1,person2]
 
